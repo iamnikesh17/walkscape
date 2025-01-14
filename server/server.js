89d40e97248c315fe8cf238e7d4720b16ba34dbc
nikesh sunam <nikeshsunam17@gmail.com>
2025-01-14 18:16:15 +0545

    fixed

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ app.use(errorHandler);
 
 
 // listen to the port  
-const PORT=process.env.PORT || 8080
+const PORT=process.env.PORT || 10000
 app.listen(PORT,()=>{
     console.log(`server is up and ruuning on port ${PORT}`);
 })
